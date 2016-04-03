01a038cb757092440cc583532bd35cdf1a483e3d
Tom Heale <tah20112@gmail.com>
2016-04-03 17:51:35 -0400

    CAN to master code

--- src/motor_concom_master.c
+++ src/motor_concom_master.c
@@ -1,14 +1,23 @@
 #define F_CPU 1000000L
 #include <avr/io.h>
 #include <util/delay.h>
+#include "inc/api.h"
+
 
 int main(void){
+
+    // Initialize CAN settings
+    int CAN_init( void );
+
+    // Set CAN message settings
+    uint8_t msg[ IDT_demo_l ];
+    msg[0] = 0xFF; // Set message to be sent as full-tilt
     
     // Set pin 10 as output for testing LED
     DDRE |= _BV(PE1);
 
     // Declare variable fro analog data
-    uint16_t analog_input;
+    uint8_t analog_input;
 
     // Define ADC sample rate
     ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
@@ -32,7 +41,8 @@ int main(void){
         analog_input = ADC;
 
         // Set LED on when input is high, low when no
-        if(analog_input > 512){
+        if(analog_input > 768){
+            CAN_Tx( IDT_demo, msg, IDT_demo_l ); // send CAN message
             PORTE |= _BV(PE1);
         }else{
             PORTE &= ~_BV(PE1);
