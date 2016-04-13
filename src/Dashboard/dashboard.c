96d9db7e33cffc84d7e382e38412462091ab0eef
Riley Chapman <rlychpmn@gmail.com>
2016-04-12 21:02:46 -0400

    moved some files around and wrote blinky code for dashboard

--- src/Dashboard/dashboard.c
+++ src/Dashboard/dashboard.c
@@ -3,42 +3,20 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include "lcd.h"
-
-ISR(CAN_INT_vect){
-    if( bit_is_set( CANSIT2, 0 )){
-        lcd_puts("MOb0 Message");
-    }
-    lcd_puts("Interrupted!");
-}
-
 
 int main(void){
-    uint8_t err=0;
+	//setup pins for blinking
+	DDRB |= _BV(PB6);
+	DDRB |= _BV(PB7);
 
-    sei();
-    lcd_init(LCD_DISP_ON_CURSOR_BLINK);
-    lcd_clrscr();
-    lcd_puts("Hello!\n");
+	PORTB |= _BV(PB6);
 
+	for(;;){
+		//toggle PB6 and PB7
+		PORTB ^= _BV(PB6);
+		PORTB ^= _BV(PB7);
 
-    CAN_init(1);
-    loop_until_bit_is_set(CANGSTA, ENFG);
-
-    lcd_puts("0x00\n");
-
-    err = CAN_Rx(0, IDT_throttle, IDT_throttle_l, IDM_single);
-    if(err){
-        lcd_puts("0x01\n");
-    }
-
-    uint8_t msg = 11;
-    err = CAN_Tx(1, IDT_demo, IDT_demo_l, &msg);
-    if(err){
-        lcd_puts("0x02\n");
-    }
-    lcd_puts("0x03\n");
-    
-    for(;;){
-    }
+		//delay for dramatic effect 
+		_delay_ms(600);
+	}
 }
